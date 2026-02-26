7d99af5722e3502bc0f9a0aed6dbd38d2cbc8e50
sin <sinc84574@gmail.com>
2026-02-26 14:10:52 +0900

    feat. 기능 추가
    - 사용자의 xxx기능을 추가했습니다
    2. 블라블라.....

--- src/main/java/com/example/lecture022/HelloController.java
+++ src/main/java/com/example/lecture022/HelloController.java
@@ -1,8 +1,13 @@
 package com.example.lecture022;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-	// 생략...
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello";
+	}
 }
